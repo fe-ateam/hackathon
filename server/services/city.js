301b2855b2855daa77fbf0dcc565093c1b199693
mpamendoza <mpamendoza@gmail.com>
2014-05-01 14:14:20 -0700

    added services for housing

--- server/services/city.js
+++ server/services/city.js
@@ -11,3 +11,18 @@ exports.getCityById = function(req, res) {
     res.send(city);
   });
 };
+
+exports.getHousingPriceSelection = function(selection, req, res) {
+  City.findOne({ _id: req.params.id }).exec(function(err, city) {
+	var housingPrice = "0";
+  	if(req.params.selection === 1){
+  		log.console('selection 1');
+	  	//housingPrice = city.category.item("Apartment (1 bedroom) in City Centre").price;
+  	}
+  	else if(req.params.selection === 2){
+  		log.console('selection 1');
+	  	//housingPrice = city.category.item("Apartment (1 bedroom) Outside of Centre").price;
+  	}
+    res.send(housingPrice);
+  });
+};
