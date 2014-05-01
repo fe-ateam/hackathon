654e4cd976a81aac059f56d28716dcb06cb4949e
mpamendoza <mpamendoza@gmail.com>
2014-05-01 16:11:14 -0700

    create service

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,4 +4,5 @@ module.exports = function(app) {
 
   app.get('/cities', cityService.getCities);
   app.get('/cities/:id', cityService.getCityById);
+  app.get('/cities/:id/:selection', cityService.getHousingPriceSelection);
 };
--- server/services/city.js
+++ server/services/city.js
@@ -12,15 +12,36 @@ exports.getCityById = function(req, res) {
   });
 };
 
-exports.getHousingPriceSelection = function(selection, req, res) {
+exports.getHousingPriceSelection = function(req, res) {
   City.findOne({ _id: req.params.id }).exec(function(err, city) {
 	var housingPrice = "0";
-  	if(req.params.selection === 1){
-  		log.console('selection 1');
-	  	//housingPrice = city.category.item("Apartment (1 bedroom) in City Centre").price;
+	var selection = req.params.selection;
+	console.log(selection);
+	var catindex = 0;
+  	if(selection === '1'){
+  		console.log('selection 1');
+  		var cats = city.categories;
+  		console.log("cats length=" + cats.length);
+  		for (var i = 0; i < cats.length; i++){
+  			console.log("cats" + i + "==" + cats[i].name);
+  			if(cats[i].name === "Rent Per Month") {
+  				console.log("HERE!!!");
+  				catindex = i;
+  			}
+  		}
+  		var items = city.categories[catindex].items;
+  		console.log("items length=" + items.length);
+  		for (var i = 0; i < items.length; i++){
+  			console.log("items" + i + "==" + items[i].name);
+  			if(items[i].name === "Apartment (1 bedroom) in City Centre"){
+  				console.log("HERE2!!!");
+  				housingPrice = items[i].price;
+  			}
+		}
+  		console.log(housingPrice);
   	}
-  	else if(req.params.selection === 2){
-  		log.console('selection 1');
+  	if(selection === '2'){
+  		console.log('selection 2');
 	  	//housingPrice = city.category.item("Apartment (1 bedroom) Outside of Centre").price;
   	}
     res.send(housingPrice);
