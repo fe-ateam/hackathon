93bb20ce54533f315d596b7dce9f79caa29179de
davidcai <w.david.cai@gmail.com>
2014-05-02 01:49:58 -0700

    fixed a bug in ring chart

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -204,7 +204,7 @@ angular.module('app')
 
 
 // Summary
-.controller('summaryController', function($scope, $log, $http) {
+.controller('summaryController', function($scope, $log, $http, $timeout) {
   $scope.pageClass = 'page-summary';
 
   $scope.total = function() {
@@ -222,56 +222,60 @@ angular.module('app')
   // Ring chart
   //
 
-  var dataset = $scope.summary;
+  $timeout(function() {
 
-  var width = 260,
-      height = 260,
-      radius = Math.min(width, height) / 2;
+    var width = 260,
+        height = 260,
+        radius = Math.min(width, height) / 2;
 
-  var pie = d3.layout.pie()
-      .sort(null)
-      .value(function(d) { return d.price; });
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function(d) { return d.price; });
 
-  var arc = d3.svg.arc()
-      .innerRadius(radius - 70)
-      .outerRadius(radius - 20);
+    var arc = d3.svg.arc()
+        .innerRadius(radius - 70)
+        .outerRadius(radius - 20);
 
-  var svg = d3.select("#ring-chart").append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    var svg = d3.select("#ring-chart").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-  var path = svg.selectAll("path")
-    .data(pie(dataset))
-    .enter()
-    .append("path");
+    var path = svg.selectAll("path")
+      .data(pie($scope.summary))
+      .enter()
+      .append("path");
 
-  path.transition()
-    .duration(500)
-    .attr("fill", function(d, i) { return $scope.pageColors[i]; })
-    .attr("d", arc)
-    .each(function(d) { this._current = d; }); // store the initial angles
-
-  // Store the displayed angles in _current.
-  // Then, interpolate from _current to the new angles.
-  // During the transition, _current is updated in-place by d3.interpolate.
-  function arcTween(a) {
-    var i = d3.interpolate(this._current, a);
-    this._current = i(0);
-    return function(t) {
-      return arc(i(t));
-    };
-  }
-
-  function updateChart(dataset) {
-    path.data(pie(dataset));
     path.transition()
-      .duration(750)
-      .attrTween("d", arcTween); // redraw the arcs
-  }
+      .duration(500)
+      .attr("fill", function(d, i) { return $scope.pageColors[i]; })
+      .attr("d", arc)
+      .each(function(d) { this._current = d; }); // store the initial angles
 
-  $scope.change = function() {
-    updateChart($scope.summary);
-  };
+    // Store the displayed angles in _current.
+    // Then, interpolate from _current to the new angles.
+    // During the transition, _current is updated in-place by d3.interpolate.
+    function arcTween(a) {
+      var i = d3.interpolate(this._current, a);
+      this._current = i(0);
+      return function(t) {
+        return arc(i(t));
+      };
+    }
+
+    function updateChart(dataset) {
+      path.data(pie(dataset));
+      path.transition()
+        .duration(750)
+        .attrTween("d", arcTween); // redraw the arcs
+    }
+
+    $scope.change = function() {
+      updateChart($scope.summary);
+    };
+
+
+  }, 500); // end of $timeout
+
 });
