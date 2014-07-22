da0be6fd64c00af66c7f74931910e01269b55fae
Ivonne Roberts <ivlo11@gmail.com>
2014-07-22 13:05:56 -0700

    first attempt to load default cities via mongoose

--- server/models/city.js
+++ server/models/city.js
@@ -27,23 +27,18 @@ function createDefaultCities() {
         console.log('Created ' + city.name);
       }
 
-      var city = new City();
-      city.name = "San Jose, CA";
-      city.category.push({name: "Restaurant", items: [{ name: "Meal, Inexpensive Restaurant", price: "8.00" },{name: "Meal for 2, Mid-range Restaurant, Three-course", price: "45.00"}]});
-      city.save(onSave);
-
-      city = new City();
-      city.name = "San Francisco";
-      city.state = "CA";
-      city.costs.push({ name: 'Meal, Inexpensive Restaurant', category: 'Food', amount: 12 });
-      city.costs.push({ name: 'Meal, Mid-range Restaurant', category: 'Food', amount: 70 });
-      city.costs.push({ name: 'Meal, Fastfood', category: 'Food', amount: 6 });
-      city.costs.push({ name: 'Utilities', category: 'Utilities', amount: 85.08, days: 30 });
-      city.costs.push({ name: '1-bedroom Apartment in City Centre', category: 'Rent', amount: 2641.73, days: 30 });
-      city.costs.push({ name: '1-bedroom Apartment outside of Centre', category: 'Rent', amount: 1838.81, days: 30 });
-      city.costs.push({ name: '3-bedroom big Apartment in City Centre', category: 'Rent', amount: 4500.00, days: 30 });
-      city.costs.push({ name: '3-bedroom big Apartment outside of Centre', category: 'Rent', amount: 3297.06, days: 30 });
-      city.save(onSave);
+      var fs = require('fs');
+      var file = __dirname + '/100citiesdefault.json';
+       
+      fs.readFile(file, 'utf8', function (err, data) {
+        if (err) {
+          console.log('Error: ' + err);
+          return;
+        }
+       
+        data = JSON.parse(data);
+        City.create(data);
+      });
     }
   })
 }
